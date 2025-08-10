d8040df502a226a5fa908ee4c444a9da736fa318
Аминов Нияз Фаилевич NiyaFAminov <niyafaminov@stud.kpfu.ru>
2025-08-10 10:13:48 +0300

    Functions in GLSL

--- ShaurmaProject/Shaders/fragment_core.glsl
+++ ShaurmaProject/Shaders/fragment_core.glsl
@@ -21,24 +21,38 @@ uniform Material material;
 uniform vec3 lightPos0;
 uniform vec3 cameraPos;
 
-void main()
+vec3 calculateAmbient(Material material)
 {
-    // Ambient light
-    float lightIntencity = 1.f;
-    vec3 ambientLight = material.ambient * lightIntencity;
-    
-    // Deffuse light
+    return material.ambient;
+}
+
+vec3 calculateDiffuse(Material material)
+{
     vec3 posToLightDirVec = normalize(lightPos0 - vs_position);
     float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
-    vec3 diffuseFinal = material.diffuse * diffuse;
-    
-    // Specular light
+    return material.diffuse * diffuse;
+}
+
+vec3 calculateSpecular(Material material)
+{
     vec3 lightToPosDirVec = normalize(lightPos0 - vs_position);
     vec3 reflectDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
     vec3 posToViewDirVec = normalize(vs_position - cameraPos);
     float specularConstant = pow(max(dot(posToViewDirVec, reflectDirVec), 0), 30);
-    vec3 specularFinal = material.specular * specularConstant;
+    return material.specular * specularConstant;
+}
+
+void main()
+{
+    // Ambient light
+    vec3 ambientFinal = calculateAmbient(material);
+    
+    // Deffuse light
+    vec3 diffuseFinal = calculateDiffuse(material);
+    
+    // Specular light
+    vec3 specularFinal = calculateSpecular(material);
     
     fs_color = texture(material.diffuseTex, vs_textcoord) * vec4(vs_color, 1.f)
-    * (vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
+    * (vec4(ambientFinal, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
 }
